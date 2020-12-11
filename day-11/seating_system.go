c18926bea9bc47f0558442d56f3318559a29d6f7
stevenhorsman <steven@uk.ibm.com>
2020-12-11 16:14:03 +0000

    Day 11 - Go
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

---
+++ day-11/seating_system.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"reflect"
+	"strings"
+)
+
+const input_file string = "./day-11/input.txt"
+
+type point struct {
+	x, y int
+}
+
+func (p point) add(d point) point {
+	return point{p.x + d.x, p.y + d.y}
+}
+
+func (p point) String() string {
+	return fmt.Sprintf("(%d, %d)", p.x, p.y)
+}
+
+func ReadInput(input string) map[point]rune {
+	grid := map[point]rune{}
+	for y, line := range strings.Split(strings.TrimSpace(input), "\n") {
+		for x, char := range line {
+			grid[point{x, y}] = char
+		}
+	}
+	return grid
+}
+
+func CountOccupied(grid map[point]rune) int {
+	count := 0
+	for _, v := range grid {
+		if v == '#' {
+			count++
+		}
+	}
+	return count
+}
+
+var deltas = []point{
+	{-1, -1},
+	{-1, 0},
+	{-1, 1},
+	{0, -1},
+	{0, 1},
+	{1, -1},
+	{1, 0},
+	{1, 1}}
+
+func Converge(grid map[point]rune, part2 bool) int {
+	crowded := 4
+	if part2 {
+		crowded = 5
+	}
+	for {
+		next := map[point]rune{}
+		// Run round rules
+		for point, seat := range grid {
+			neighbours := 0
+			for _, d := range deltas {
+				adj := point.add(d)
+				if part2 {
+					for grid[adj] == '.' {
+						adj = adj.add(d)
+					}
+				}
+				if grid[adj] == '#' {
+					neighbours++
+				}
+			}
+			if seat == '#' && neighbours >= crowded {
+				next[point] = 'L'
+			} else if seat == 'L' && neighbours == 0 {
+				next[point] = '#'
+			} else {
+				next[point] = seat
+			}
+		}
+		if reflect.DeepEqual(grid, next) {
+			return CountOccupied(grid)
+		}
+		grid = next
+	}
+}
+
+func Part1(input string) int {
+	grid := ReadInput(input)
+	return Converge(grid, false)
+}
+
+func Part2(input string) int {
+	grid := ReadInput(input)
+	return Converge(grid, true)
+}
+
+func main() {
+	b, err := ioutil.ReadFile(input_file)
+	if err != nil {
+		fmt.Print(err)
+	}
+	data := string(b)
+
+	fmt.Printf("Part 1: %d\n", Part1(data))
+	fmt.Printf("Part 2: %d\n", Part2(data))
+}
---
+++ day-11/seating_system_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+func TestPart1Example1(t *testing.T) {
+	data := `
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL`[1:]
+	expected := 37
+	actual := Part1(data)
+	if actual != expected {
+		t.Errorf("Expected %d but was %d\n", expected, actual)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	b, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	data := string(b)
+
+	expected := 2178
+	actual := Part1(data)
+	if actual != expected {
+		t.Errorf("Expected %d but was %d\n", expected, actual)
+	}
+}
+
+func TestPart2Example1(t *testing.T) {
+	data := `
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL`[1:]
+	expected := 26
+	actual := Part2(data)
+	if actual != expected {
+		t.Errorf("Expected %d but was %d\n", expected, actual)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	b, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	data := string(b)
+
+	expected := 1978
+	actual := Part2(data)
+	if actual != expected {
+		t.Errorf("Expected %d but was %d\n", expected, actual)
+	}
+}
