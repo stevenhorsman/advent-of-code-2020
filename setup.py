26503bd53a949220024318dd12f2ec5a80d55f74
stevenhorsman <steven@uk.ibm.com>
2021-01-05 16:55:48 +0000

    Update linter to pylint
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
-import sys
-import shutil
-import os
-import requests
 import argparse
 from datetime import date
+import os
+import requests
 
 def download_input(year, day, filename):
   input_url='https://adventofcode.com/'+str(year)+'/day/'+str(int(day))+'/input'
@@ -16,7 +14,6 @@ def download_input(year, day, filename):
   file = open(filename, "w")
   file.write(response.text.rstrip("\r\n"))
   file.close()
-
 
 def create_py(directory, name, day):
   create_py_contents(directory + os.path.sep + name + ".py", day.zfill(2))
@@ -39,18 +36,17 @@ def main():
   year = args.year
   is_go = args.go
 
-  day_string = 'day-' + day.zfill(2)
+  day_string = 'day_' + day.zfill(2)
   if not is_go:
     print("Creating day %s with name %s" % (day, name))
-  
+
     os.mkdir(day_string)
     create_py(day_string, name, day)
     download_input(year, day, day_string+os.path.sep+"input.txt")
   else:
     create_go(day_string, name, day)
-   
 
-py_file = """input_file = 'day-{{day}}/input.txt'
+PY_FILE = """INPUT_FILE = 'day_{{day}}/input.txt'
 
 def part1(input):
   return 0
@@ -59,14 +55,14 @@ def part2(input):
   return 0
 
 if __name__ == "__main__":
-  with open(input_file) as f:
+  with open(INPUT_FILE) as f:
     data = f.read()
   print("Part 1: ", part1(data))
   print("Part 2: ", part2(data))"""
 def create_py_contents(filename, day_no):
-    create_file_contents(filename, py_file, '{{day}}', day_no)
+  create_file_contents(filename, PY_FILE, '{{day}}', day_no)
 
-test_file = """import {{name}}, fileinput
+TEST_FILE = """import {{name}}, fileinput
 
 def test_part1_example_1():
   data = '''
@@ -79,7 +75,7 @@ def test_part1_example_2():
   assert {{name}}.part1(data) == -1
 
 def test_part1():
-  with open({{name}}.input_file) as f:
+  with open({{name}}.INPUT_FILE) as f:
     data = f.read()
   expected = -1
   assert {{name}}.part1(data) == expected
@@ -95,15 +91,15 @@ def test_part2_example_2():
   assert {{name}}.part2(data) == -1
 
 def test_part2():
-  with open({{name}}.input_file) as f:
+  with open({{name}}.INPUT_FILE) as f:
     data = f.read()
   expected = -1
   assert {{name}}.part2(data) == expected
 """
 def create_test_contents(filename, name):
-    create_file_contents(filename, test_file, '{{name}}', name)
+  create_file_contents(filename, TEST_FILE, '{{name}}', name)
 
-go_file = """package main
+GO_FILE = """package main
 
 import (
 	"fmt"
@@ -113,7 +109,7 @@ import (
 	"strings"
 )
 
-const inputFile string = "./day-{{day}}/input.txt"
+const inputFile string = "./day_{{day}}/input.txt"
 
 //Part1 - Solution to Part 1 of the puzzle
 func Part1(input string) int {
@@ -137,9 +133,9 @@ func main() {
 }"""
 
 def create_go_contents(filename, day_no):
-    create_file_contents(filename, go_file, '{{day}}', day_no)
+  create_file_contents(filename, GO_FILE, '{{day}}', day_no)
 
-go_test_file = """package main
+GO_TEST_FILE = """package main
 
 import (
 	"fmt"
@@ -212,15 +208,15 @@ func BenchmarkPart2(b *testing.B) {
 }"""
 
 def create_go_test_contents(filename):
-    create_file_contents(filename, go_test_file, "<none>", "<none>")
+  create_file_contents(filename, GO_TEST_FILE, "<none>", "<none>")
 
 def create_file_contents(filename, contents, replaced, replace):
-    # Replace the target string
-    filedata = contents.replace(replaced, replace)
+  # Replace the target string
+  filedata = contents.replace(replaced, replace)
 
-    # Write the file out again
-    with open(filename, 'w') as file:
-        file.write(filedata)
+  # Write the file out again
+  with open(filename, 'w') as file:
+    file.write(filedata)
 
 if __name__ == '__main__':
-    main()
+  main()
